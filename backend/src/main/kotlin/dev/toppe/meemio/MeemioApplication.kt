02cbc3d9dd002ee097833297254f0f00c2f57d1d
toppev <44984598+toppev@users.noreply.github.com>
2020-03-27 12:47:37 +0200

    Initial commit

---
+++ backend/src/main/kotlin/dev/toppe/meemio/MeemioApplication.kt
@@ -0,0 +1,11 @@
+package dev.toppe.meemio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeemioApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeemioApplication>(*args)
+}
---
+++ backend/src/test/kotlin/dev/toppe/meemio/MeemioApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.toppe.meemio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeemioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
